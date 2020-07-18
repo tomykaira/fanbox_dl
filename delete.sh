d9b6c059f01ec91dd0b299adcfe7ad5575e61412
whoami <whoami@example.com>
2020-07-18 14:51:27 +0900

    Support multiple delete

--- delete.sh
+++ delete.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-id=$1
-
-curl 'https://api.fanbox.cc/post.delete' \
-	-H 'Content-Type: application/json' \
-	-H "Origin: https://$CREATOR_ID.fanbox.cc" \
-	-H "X-CSRF-Token: $CSRF_TOKEN" \
-	-H "Cookie: FANBOXSESSID=$SESSID" \
-	--data-raw '{"postId":"'$id'"}'
+for id in $*; do
+	curl 'https://api.fanbox.cc/post.delete' \
+		-H 'Content-Type: application/json' \
+		-H "Origin: https://$CREATOR_ID.fanbox.cc" \
+		-H "X-CSRF-Token: $CSRF_TOKEN" \
+		-H "Cookie: FANBOXSESSID=$SESSID" \
+		--data-raw '{"postId":"'$id'"}'
+done
